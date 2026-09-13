66649c60267d15181eec9c6cb1567a022392a235
Eddyphonelandia <eddybor@msn.com>
2026-09-13 23:53:04 +0200

    Fix import formatting in location_picker_screen.dart

--- lib/features/saved_places/presentation/location_picker_screen.dart
+++ lib/features/saved_places/presentation/location_picker_screen.dart
@@ -1,4 +1,4 @@
-import '../../../core/config/map_style_path.dart'
+import '../../../core/config/map_style_path.dart';
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 
